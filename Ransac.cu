666e0edbbeb87c46a9c8d76ab1e308ed77c4a7a2
Varga Bence <wben0802@gmail.com>
2022-12-10 21:27:22 +0100

    read from file

--- Ransac.cu
+++ Ransac.cu
@@ -1,3 +1,11 @@
 #include "Ransac.cuh"
 
+#include <fstream>
+#include <iterator>
+
 Ransac::Ransac(int iteration, float error) : iteration(iteration), error(error) {}
+
+void Ransac::read_file(const char *filename, std::vector<Point> &dataPoints) {
+    std::ifstream inputstream(filename);
+    dataPoints.assign(std::istream_iterator<Point>(inputstream), std::istream_iterator<Point>());
+}
--- Ransac.cuh
+++ Ransac.cuh
@@ -1,13 +1,19 @@
 #ifndef CIRCLE_DETECTION_CUDA_RANSAC_CUH
 #define CIRCLE_DETECTION_CUDA_RANSAC_CUH
 
+#include <vector>
+
+#include "Point.cuh"
 
 class Ransac {
 public:
-    Ransac(int iteration, float error);
+     Ransac() = default;
+     Ransac(int iteration, float error);
 private:
     int iteration;
     float error;
+
+    void read_file(const char* filename,std::vector<Point> &dataPoints);
 };
 
 
