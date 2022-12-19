73246ea505fe5267eb6ec86616517260dbf5bd4d
Varga Bence <wben0802@gmail.com>
2022-12-19 14:28:03 +0100

    simplify the main function

--- src/main.cu
+++ src/main.cu
@@ -4,12 +4,17 @@
 
 int main(int argc , char** argv)
 {
-    if (argc != 4) {
-        fmt::print(stderr, "Usage: cirle_detection file.text iteration error");
+
+    if (argc == 2){
+        Ransac ransac;
+        ransac.run(argv[1]);
+    } else if (argc == 4){
+        Ransac ransac(std::stoi(argv[2]), std::stof(argv[3]));
+        ransac.run(argv[1]);
+    } else {
+        fmt::print(stderr, "Usage: circle_detection file.text iteration error\n"
+                           "Usage: circle_detection file.text");
         return EXIT_FAILURE;
     }
-
-    Ransac ransac(std::stoi(argv[2]), std::stof(argv[3]));
-    ransac.run(argv[1]);
     return 0;
 }
